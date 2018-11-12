6a1f5fac89f6e22abfaa01badf2898a269b59649
Chuk <chukwumaokpalugo@hotmail.com>
2018-11-12 00:16:36 -0500

    Updated ignore

---
+++ assets/bundles/precache-manifest.d36b12622fda09cf0a72c337b1c47946.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "c29cf47b4c1930aed175",
+    "url": "/static/bundles/js/main.c29cf47b.js"
+  },
+  {
+    "revision": "5048c093d3cec88424c58dce31c5360b",
+    "url": "/static/bundles/index.html"
+  },
+  {
+    "revision": "c29cf47b4c1930aed175",
+    "url": "/static/bundles/css/main.5ad7e58a.css"
+  }
+];
